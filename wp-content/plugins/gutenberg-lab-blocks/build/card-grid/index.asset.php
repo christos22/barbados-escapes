98ee2e5f77bf7265c7209b0a03cf277c694bcb46
Christos <christos@grapsas.dev>
2026-07-01 22:37:40 -0400

    Fix card grid column selection

--- wp-content/plugins/gutenberg-lab-blocks/build/card-grid/index.asset.php
+++ wp-content/plugins/gutenberg-lab-blocks/build/card-grid/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-html-entities', 'wp-i18n', 'wp-server-side-render'), 'version' => '9da8fd7939649981bb69');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-html-entities', 'wp-i18n', 'wp-server-side-render'), 'version' => '49b22956516cd184077e');
