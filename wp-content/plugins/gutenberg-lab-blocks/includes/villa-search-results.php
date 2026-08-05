efede0e75231e11adb48970be88b92fbc3252475
Christos <christos@grapsas.dev>
2026-08-05 17:27:01 -0400

    Polish villa search layout and result cards

--- wp-content/plugins/gutenberg-lab-blocks/includes/villa-search-results.php
+++ wp-content/plugins/gutenberg-lab-blocks/includes/villa-search-results.php
@@ -532,10 +532,6 @@ function gutenberg_lab_blocks_render_villa_search_results_markup( $wrapper_attri
 					<?php endforeach; ?>
 				</ul>
 			<?php endif; ?>
-
-			<a class="vvm-villa-search-results__adjust" href="#villa-search">
-				<?php esc_html_e( 'Adjust your search', 'gutenberg-lab-blocks' ); ?>
-			</a>
 		</header>
 
 		<?php if ( '' !== $card_markup ) : ?>
