3739d19319ed2e52e22cef422a2098bd5a5ab74b
Christos <christos@grapsas.dev>
2026-08-23 19:01:45 -0400

    Fix homepage villa outline buttons

--- wp-content/plugins/gutenberg-lab-blocks/build/card-grid/index.asset.php
+++ wp-content/plugins/gutenberg-lab-blocks/build/card-grid/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-html-entities', 'wp-i18n', 'wp-server-side-render'), 'version' => '5d219e3468e89bddcf8f');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-html-entities', 'wp-i18n', 'wp-server-side-render'), 'version' => 'a0cb5e943f942549cf55');
