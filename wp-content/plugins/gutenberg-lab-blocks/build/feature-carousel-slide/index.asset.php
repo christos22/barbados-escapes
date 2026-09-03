534a1d2881444f08db75c0dc772cffb602f23918
Christos <christos@grapsas.dev>
2026-09-03 00:54:34 -0400

    Apply Italiana treatment to all headings

--- wp-content/plugins/gutenberg-lab-blocks/build/feature-carousel-slide/index.asset.php
+++ wp-content/plugins/gutenberg-lab-blocks/build/feature-carousel-slide/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'cbefe44054ee45536103');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '601bb60e750469acc557');
--- wp-content/plugins/gutenberg-lab-blocks/build/villa-gallery-hero-content/index.asset.php
+++ wp-content/plugins/gutenberg-lab-blocks/build/villa-gallery-hero-content/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-i18n'), 'version' => '07e9050f3725745416ca');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-i18n'), 'version' => 'c4a19a77c678173ec68a');
